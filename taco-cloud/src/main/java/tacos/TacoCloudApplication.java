35991d7d6cfc2905625a0c65bc058e3a231bc890
hzyangyizhou <yangyizhou@weipugroup.com>
2024-07-19 20:15:31 +0800

    spring起步，p77

---
+++ taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args); // 执行应用的引导过程
+    }
+
+}
